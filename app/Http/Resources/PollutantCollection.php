8deefa7c6ba07328b6c653c22058f0cce14c7dd3
Stéphane Bour <sbour@welcome-media.fr>
2018-10-23 21:20:58 +0200

    style: php-cs-fixer

--- app/Http/Resources/PollutantCollection.php
+++ app/Http/Resources/PollutantCollection.php
@@ -9,7 +9,8 @@ class PollutantCollection extends ResourceCollection
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function toArray($request)
